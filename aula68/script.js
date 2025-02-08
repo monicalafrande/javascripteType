79e38f57f5ade2ab19fc95f794fe207f0f3aa48d
monicalfrande <monicalafrande@gmail.com>
2025-02-08 15:33:22 -0300

    Treinando os tipos de funções

---
+++ aula68/script.js
@@ -0,0 +1,64 @@
+// function falaOi() {
+//     console.log('Oi');
+// }
+// falaOi();
+
+// const falaOi2 = () => {
+//     console.log('Oi');
+// }
+// falaOi2();
+
+// const pessoa = {
+//     fala() {
+//         console.log('Oi');
+//     }
+// }
+// pessoa.fala();
+
+// function fala() {
+//   console.log("Oi");
+// }
+// fala();
+
+// const fala2 = () => console.log("Oi");
+// fala2();
+
+// const pessoa = {
+//   fala() {
+//     console.log("Oiii");
+//   },
+// };
+// pessoa.fala();
+
+// Função normal
+// function fala() {
+//     console.log("Oi");
+// }
+// fala();
+
+// // Arrow function
+// const fala2 = () => console.log("Oii");
+// fala2();
+
+// // Como um método de objeto
+// const pessoa = {
+//     fala3() {
+//         console.log("Oiii");
+//     }
+// }
+// pessoa.fala3();
+
+function fala() {
+    console.log("Oi");
+}
+fala();
+
+const fala2 = () => console.log("Oii");
+fala2();
+
+const pessoa = {
+    fala3() {
+        console.log("Oiii");
+    }
+}
+pessoa.fala3();
